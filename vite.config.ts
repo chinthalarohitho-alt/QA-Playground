2d4b58e1f2f5f1249844dde7bf21dda82c76c267
rohithkumaro <rohith.kumar@yellow.ai>
2025-11-26 18:30:07 +0530

    Prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
+  
   return {
+    base: "/QA-Playground/",
     server: {
       port: 5173,
       host: '0.0.0.0',
